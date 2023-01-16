23f160edeaca76b49ce07bb159562ee4743a8454
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2023-01-16 15:11:52 +0100

    Add experiment

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({origin: "https://testrender-fe.onrender.com",credentials: true}))
+// Cookie handling test
+app.use(cors({ origin: "https://testrender-fe.onrender.com", credentials: true }))
 app.use(cookieParser())
-
 app.use((req, res) => {
   console.log(req.cookies)
   const cookieOptions = {
-    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
     sameSite: "None",
   }
-  res.cookie("cooki", "I am a horse" + Math.round(Math.random() * 10000), cookieOptions)
-  res.send("potato, " + JSON.stringify(req.cookies))
+  if (!req.cookies.session) {
+    res.cookie("session", "sessid_" + Date.now(), cookieOptions)
+    res.send("Welcome!")
+  } else {
+    res.send("Welcome back agent " + req.cookies.session)
+  }
 })
 
+// Setup
 const port = process.env.PORT || 5000
 const server = app.listen(port, () => console.log("Listening on port: " + port))
-
 process.on("unhandledRejection", err => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down...")
   console.log(err)
